e5cf6d97e0d349b65ae9eb3999998eafaaab477c
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-31 20:44:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,18 +3,18 @@ def join_nested_strings(src)
   new_array = []
   row_index = 0 
   string_variable = ""
+  hold_string = 0
   
   while row_index < src.count do 
     element_index = 0 
     
     while element_index < src[row_index].count do 
       if src[row_index][element_index] == string_variable.class
-        
-        
+         hold_string = src[row_index][element_index]
       end   
-    
+       element_index += 1 
     end
-    
+    row_index += 1 
   end 
   
 end
